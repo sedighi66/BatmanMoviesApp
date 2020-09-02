2004a65e1ee71108fc672e62f5878b8f4b511a02
Mohsen Sedighi <sedighi.66@gmail.com>
2020-09-02 17:04:07 +0430

    viewModel is created.

--- app/src/main/java/org/msfox/batmanmoviesapp/di/ViewModelModule.kt
+++ app/src/main/java/org/msfox/batmanmoviesapp/di/ViewModelModule.kt
@@ -6,6 +6,7 @@ import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 import org.msfox.batmanmoviesapp.vm.AppViewModelFactory
+import org.msfox.batmanmoviesapp.vm.DescriptionViewModel
 import org.msfox.batmanmoviesapp.vm.SearchMoviesViewModel
 
 @Suppress("unused")
@@ -19,4 +20,9 @@ abstract class ViewModelModule {
     @IntoMap
     @ViewModelKey(SearchMoviesViewModel::class)
     abstract fun bindSearchMoviesViewModel(searchMoviesViewModel: SearchMoviesViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(DescriptionViewModel::class)
+    abstract fun bindDescriptionViewModel(descriptionViewModel: DescriptionViewModel): ViewModel
 }
--- app/src/main/java/org/msfox/batmanmoviesapp/vm/DescriptionViewModel.kt
+++ app/src/main/java/org/msfox/batmanmoviesapp/vm/DescriptionViewModel.kt
@@ -2,21 +2,17 @@ package org.msfox.batmanmoviesapp.vm
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import org.msfox.batmanmoviesapp.model.Description
 import org.msfox.batmanmoviesapp.model.Movie
+import org.msfox.batmanmoviesapp.repository.DescriptionRepository
 import org.msfox.batmanmoviesapp.repository.Resource
 import org.msfox.batmanmoviesapp.repository.SearchMoviesRepository
 import javax.inject.Inject
 
-class SearchMoviesViewModel @Inject constructor(
-    private val repo: SearchMoviesRepository): ViewModel()  {
+class DescriptionViewModel @Inject constructor(
+    private val repo: DescriptionRepository): ViewModel()  {
 
-    init {
-        //we assume the capability to search for movies from api.
-        //but for this test project in wasn't in the requirement.
-        //so we just hard code query to batman.
-        repo.setQuery("batman")
+    fun getDescription(imdbId: String): LiveData<Resource<Description>> {
+        return repo.getDescription(imdbId)
     }
-
-    fun loadData() = repo.loadMoreData()
-    fun getMovies(): LiveData<Resource<List<Movie>>> = repo.getMovies()
 }
